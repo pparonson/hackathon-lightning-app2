97546555510939a8a660883490cd89c355bff128
paronson <paronson@qw-corp.com>
2022-08-06 10:21:10 -0500

    remove stuff

--- backend/out/src/shared/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.SocketEvents = void 0;
-exports.SocketEvents = {
-    postUpdated: 'post-updated',
-    invoicePaid: 'invoice-paid'
-};
-//# sourceMappingURL=types.js.map
